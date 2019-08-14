598b6efd409713149533a32dca8bda4686b45ee8
Fernanda <fernanda.buz@gmail.com>
2019-08-14 13:39:36 -0300

    Defer

--- funcoes/defer/defer.go
+++ funcoes/defer/defer.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import "fmt"
 
+func obterValorAprovado(numero int) int {
+	defer fmt.Println("Fim!") //atrasa execução do código para até antes de sair do método
+	if numero >= 5000 {
+		fmt.Println("Valor alto...")
+		return 5000
+	}
+	fmt.Println("Valor baixo...")
+	return numero
+}
+
+func main() {
+	fmt.Println(obterValorAprovado(6000))
 }
