a845d37501348d0accc705daeb322a8fe71d5460
fernandabuz <fernanda.buz@gmail.com>
2019-08-16 09:24:51 -0300

    json

--- tipos/json/json.go
+++ tipos/json/json.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
 type produto struct {
 	ID    int      `json:"id"` //identificador começando com letra maiúscula tem significado de ser público e minúscula é privado
 	Nome  string   `json:"nome"`
@@ -8,5 +13,14 @@ type produto struct {
 }
 
 func main() {
+	//struct para json
+	p1 := produto{1, "Notebook", 99.0, []string{"promo", "eletro"}}
+	p1Json, _ := json.Marshal(p1) //retorna o json e o erro - sempre dois
+	fmt.Println(string(p1Json))
 
+	//json para struct
+	var p2 produto
+	jsonString := `{"id": 2, "nome": "Celular", "preco": 1800.00, "tags": ["baratinho", "novinho"]}`
+	json.Unmarshal([]byte(jsonString), &p2) //primeiro slice de byte passando referencia do p2 - para que ele leia o json e sete os valores pra dentro do p2(por referencia &)
+	fmt.Println(p2.Tags[1])
 }
