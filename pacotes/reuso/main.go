c0d4d470de5f18b054cff2c611ab247690e3f108
fernandabuz <fernanda.buz@gmail.com>
2019-08-16 10:13:01 -0300

    reuso

---
+++ pacotes/reuso/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/fernandabuz/area"
+)
+
+func main() {
+	fmt.Println(area.Circ(6.0))
+	fmt.Println(area.Rect(2.0, 4.0))
+	//fmt.Println(area._TrianguloEq(10.0, 2.0))
+}
